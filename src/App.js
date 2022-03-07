cf449553f0b82aebf702420d179f7262329106b6
David Redding <redding.david35@gmail.com>
2022-03-07 08:27:21 -0600

    added nav + links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const App = () => (
   <div>
     <h1>Bookkeeper!</h1>
+    <nav>
+      <Link to={'/invoices'}>Invoices</Link> | <Link to={'/expenses'}>Expenses</Link>
+    </nav>
   </div>
 );
 
