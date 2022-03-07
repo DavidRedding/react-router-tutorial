e69746f96bfe56b24acc31e4a2e9b619889784f3
David Redding <redding.david35@gmail.com>
2022-03-07 08:38:32 -0600

    built expenses & invoices routes

---
+++ src/routes/expenses.js
@@ -0,0 +1,7 @@
+const Expenses = () => (
+  <main>
+    <h2 className="text-teal-500">Expenses</h2>
+  </main>
+);
+
+export default Expenses;
---
+++ src/routes/invoices.js
@@ -0,0 +1,7 @@
+const Invoices = () => (
+  <main>
+    <h2 className="text-rose-500">Invoices</h2>
+  </main>
+);
+
+export default Invoices;
