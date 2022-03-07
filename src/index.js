2f9a3514531a69e9fcc70ddcad34b7dc9610d68a
David Redding <redding.david35@gmail.com>
2022-03-07 08:55:31 -0600

    created route config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Expenses from './routes/Expenses';
+import Invoices from './routes/Invoices';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
   </BrowserRouter>,
   document.getElementById('root')
 );
